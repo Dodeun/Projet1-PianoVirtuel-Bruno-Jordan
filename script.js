71d8a4eb434f5f433db06b385f2c28e309e91a22
Burdon Jordan <burdon.jordan@gmail.com>
2025-03-13 17:27:30 +0100

    jouer note clavier

--- script.js
+++ script.js
@@ -13,6 +13,23 @@ const piano1Path = "assets/sounds/piano1";
 /* valeur par défaut du volume du clavier */
 const pianoVolume = 0.5;
 
+const audioArray = [];
+
+const keyboardReferences = {
+  Q: 0,
+  Z: 1,
+  S: 2,
+  E: 3,
+  D: 4,
+  F: 5,
+  T: 6,
+  G: 7,
+  Y: 8,
+  H: 9,
+  U: 10,
+  J: 11,
+};
+
 /* creer et assigne un element audio pour chaque touche de piano, en fonction du choix de piano */
 function setupPianoAudios(keysList, pianoPathInput, volumeInput) {
   /* boucle sur chaque touche de la liste de touches */
@@ -20,6 +37,7 @@ function setupPianoAudios(keysList, pianoPathInput, volumeInput) {
     /* creer un new audio qui à pour source le chemin donné dans 
         pianoPath + le nom du fichier qui correspond au data-note */
     const audioKey = new Audio(`${pianoPathInput}/${key.dataset.note}.mp3`);
+    audioArray.push(audioKey);
 
     /* ajuste le volume de la note en fonction des options du piano */
     audioKey.volume = volumeInput;
@@ -39,8 +57,20 @@ function playOnClick(clickedKey) {
   /* mets le "player" de la note clické a 0.5 secondes du debut */
   clickedKey.currentTime = 0.5;
 
-  /* joues le note */
+  /* joues la note */
   clickedKey.play();
 }
+
+/* jouer la note correspond a la touche clavier  */
+function playOnKeyboardPress(clickedKeyboardKey) {
+  const note = audioArray[keyboardReferences[clickedKeyboardKey]];
+  note.currentTime = 0.5;
+  note.play();
+  /* audioArray[keyboardReferences.clickedKeyboardKey].play(); */
+}
+
+body.addEventListener("keypress", (e) => {
+  playOnKeyboardPress(e.key.toUpperCase());
+});
 
 setupPianoAudios(keysArray, piano1Path, pianoVolume);
